cc3b133b714cc9209c8190c9c00d14bd1c18efc9
Alexis Zeballos <97553981+AlexisWorkSpace@users.noreply.github.com>
2022-08-26 14:27:34 -0300

    Create calculadoraVertices.c
    esta aplicacion en C es capaz de calcular los puntos del vertice en ambos ejes cartecianos, de una funcion lineal

---
+++ calculadoraVertices.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+#include<math.h>
+
+typedef struct {
+        float cuadratic = 0;
+        float lineal = 0;
+        float independent = 0;
+} function;
+
+float horizontal_vertice(float, float);
+float vertical_vertice(float, float, float, float);
+
+int main(){
+
+        function coefficient;
+
+        printf("ingrese el coheficiente cuadratico-> "); scanf("%f", &coefficient.cuadratic);
+        printf("ingrese el coheficiente lineal-> "); scanf("%f", &coefficient.lineal);
+        printf("ingrese el coheficiente independiente-> "); scanf("%f", &coefficient.independent);
+
+        float hVertices = horizontal_vertice(coefficient.cuadratic, coefficient.lineal);
+        float vVertices = vertical_vertice(coefficient.cuadratic, coefficient.lineal, coefficient.independent, hVertices);
+
+        printf("vertice en eje x-> %.2f\n", hVertices);
+        printf("vertice en eje y-> %.2f\n", vVertices);
+        return 0;
+}
+
+float horizontal_vertice(float a, float b) {
+        return (-b)/(2 * a);
+}
+
+float vertical_vertice(float a, float b, float c, float horizontal_vertice){
+        return (a * pow(horizontal_vertice, 2) + b * horizontal_vertice + c);
+}
